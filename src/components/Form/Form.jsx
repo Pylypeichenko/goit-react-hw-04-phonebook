dd1db016ef6d03713a6d6d83e2ae119adcb1c095
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-10-26 12:40:01 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -19,7 +19,6 @@ export class Form extends Component {
     const form = e.currentTarget;
     this.props.onFormSubmit(this.state);
 
-    form.reset();
     this.reset();
   };
 
@@ -35,6 +34,7 @@ export class Form extends Component {
           id="contactName"
           type="text"
           name="name"
+          value={this.state.name}
           onChange={this.handleChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -46,6 +46,7 @@ export class Form extends Component {
           id="contactTel"
           type="tel"
           name="number"
+          value={this.state.number}
           onChange={this.handleChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
